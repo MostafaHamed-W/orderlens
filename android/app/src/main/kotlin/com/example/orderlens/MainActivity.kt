9653ac8987aaeb422e5fde589d21e9969b0e0b98
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-12-19 16:23:00 +0200

    order lens starter project

---
+++ android/app/src/main/kotlin/com/example/orderlens/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.orderlens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:orderlens/order_lens.dart';
+
+void main() {
+  runApp(const OrderLens());
+}
