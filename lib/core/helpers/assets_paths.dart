dd71d125d99f2fc12047dcfe3b68789e0339b0a2
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-12-20 23:11:18 +0200

    added local data service

--- lib/core/helpers/assets_paths.dart
+++ lib/core/helpers/assets_paths.dart
@@ -1,6 +1,9 @@
-class AppImages {
+class AssetsPaths {
+  static const String orderJson = 'lib/features/dashboard/data/sources/orders.json';
   static const String coreImagesPath = 'assets/images/';
 
   /// OnBoarding
   static const String onBoarding = '${coreImagesPath}onboarding.svg';
+
+
 }
