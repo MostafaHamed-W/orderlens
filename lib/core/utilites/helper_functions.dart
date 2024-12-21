3152f2d190bd344173e7ac81563d1b888bf0ee82
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-12-21 06:58:26 +0200

    added essential extensions and helper functions

---
+++ lib/core/utilites/helper_functions.dart
@@ -0,0 +1,16 @@
+import 'package:orderlens/features/dashboard/data/models/grouped_orders_model.dart';
+
+class HelperFunctions {
+  // Helper method to get the years of orders
+  static String getYears(List<GroupedOrdersModel> groupedOrders) {
+    // Get the years of the orders
+    // Using set to make year unique
+    final years = groupedOrders.map((order) => order.date.year).toSet().toList()..sort();
+    if (years.length == 1) {
+      return years.first.toString();
+    } else if (years.length > 1) {
+      return '${years.first} - ${years.last}';
+    }
+    return '';
+  }
+}
